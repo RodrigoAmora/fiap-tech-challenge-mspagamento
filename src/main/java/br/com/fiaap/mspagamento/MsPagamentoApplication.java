1f682858e71b8e0d6bf1fcb7d635d752f7a15cb4
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-07-19 22:17:54 -0300

    Commit do Projeto

---
+++ src/main/java/br/com/fiaap/mspagamento/MsPagamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiaap.mspagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPagamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPagamentoApplication.class, args);
+    }
+
+}
