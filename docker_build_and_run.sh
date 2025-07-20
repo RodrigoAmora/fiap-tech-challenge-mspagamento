dab0bc6cf93b0fac1cf98b8fe76053ba1d7b29eb
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-07-19 22:43:13 -0300

    Shellscript para rodar o Docker

---
+++ docker_build_and_run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#############################################
+### Shellscript to up Docker's containers ###
+#############################################
+
+### Application ###
+rm -rf target/
+mvn clean install -DskipTests
+
+### Docker ###
+echo -e "\n\n"
+echo -e "\033[01;32m##############\033[01;32m"
+echo -e "\033[01;32m### Docker ###\033[01;32m"
+echo -e "\033[01;32m##############\033[01;32m"
+echo -e "\n"
+
+sudo docker rmi -f rodrigoamora/rodrigo-springboot
+
+echo -e "\n\n"
+echo -e "\033[01;32m###########################\033[01;32m"
+echo -e "\033[01;32m### Building images.... ###\033[01;32m"
+echo -e "\033[01;32m###########################\033[01;32m"
+echo -e "\n\n"
+
+sudo docker-compose build
+
+echo -e "\n\n"
+echo -e "\033[01;32m########################\033[01;32m"
+echo -e "\033[01;32m### Uping containers ###\033[01;32m"
+echo -e "\033[01;32m########################\033[01;32m"
+echo -e "\n\n"
+
+sudo docker-compose up -d
+
+echo -e "\n"
+echo -e "\033[01;32m###############################\033[01;32m"
+echo -e "\033[01;32m### Application running!!!! ###\033[01;32m"
+echo -e "\033[01;32m###############################\033[01;32m"
+
