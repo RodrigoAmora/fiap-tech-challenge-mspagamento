fffb66c2c1c7852a2184f3af1d66176e939b8e6f
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-07-20 01:29:42 -0300

    Status de Pagamento

--- src/main/java/br/com/fiaap/mspagamento/domain/StatusPagamento.java
+++ src/main/java/br/com/fiaap/mspagamento/domain/StatusPagamento.java
@@ -1,4 +1,29 @@
 package br.com.fiaap.mspagamento.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum StatusPagamento {
+
+    RECEBIDO("RECEBIDO"),
+    CONFIRMADO("CONFIRMADO"),
+    NEGADO("NEGADO"),
+    CANCELADO("CANCELADO");
+
+    private String status;
+
+    StatusPagamento(String status) {
+        this.status = status;
+    }
+
+    @JsonValue
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
 }
