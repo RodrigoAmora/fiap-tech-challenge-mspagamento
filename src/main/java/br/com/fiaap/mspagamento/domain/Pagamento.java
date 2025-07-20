c6cfe25cee5e2becfefdec02a166c60e76f1ea9c
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-07-20 01:31:58 -0300

    Classe Pagamento

--- src/main/java/br/com/fiaap/mspagamento/domain/Pagamento.java
+++ src/main/java/br/com/fiaap/mspagamento/domain/Pagamento.java
@@ -4,29 +4,35 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record Pagamento(
+@Document(collection = "pagamentos")
+@Data
+public class Pagamento {
+
         @JsonProperty("id")
-        UUID idPagamento,
+        UUID idPagamento;
 
         @JsonProperty("id_pedido")
-        UUID idPedido,
+        UUID idPedido;
 
         @JsonProperty("id_cliente")
-        UUID idCliente,
+        UUID idCliente;
 
         @JsonProperty("total")
-        BigDecimal valorTotal,
+        BigDecimal valorTotal;
 
         @Enumerated(EnumType.STRING)
         @JsonProperty("status")
-        StatusPagamento statusPagamento,
+        StatusPagamento statusPagamento;
 
         @JsonProperty("data")
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-        LocalDateTime dataPagamento
-) {}
+        LocalDateTime dataPagamento;
+
+}
