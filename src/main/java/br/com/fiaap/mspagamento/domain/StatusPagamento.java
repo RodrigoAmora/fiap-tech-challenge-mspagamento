3d647c6287c058cf2711a7ef34f6ac160d83ab6d
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-07-20 02:00:25 -0300

    Status do Pagamento

--- src/main/java/br/com/fiaap/mspagamento/domain/StatusPagamento.java
+++ src/main/java/br/com/fiaap/mspagamento/domain/StatusPagamento.java
@@ -2,14 +2,25 @@ package br.com.fiaap.mspagamento.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonValue;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(
+        description = "Status possíveis de um pagamento"
+)
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum StatusPagamento {
 
-    RECEBIDO("RECEBIDO"),
-    CONFIRMADO("CONFIRMADO"),
-    NEGADO("NEGADO"),
-    CANCELADO("CANCELADO");
+    @Schema(description = "Pagamento aprovado com sucesso")
+    APROVADO("APROVADO"),
+
+    @Schema(description = "Pagamento em processamento")
+    PROCESSANDO("PROCESSANDO"),
+
+    @Schema(description = "Pagamento ainda não processado")
+    PENDENTE("PENDENTE"),
+
+    @Schema(description = "Pagamento foi rejeitado")
+    REJEITADO("REJEITADO");
 
     private String status;
 
--- src/main/java/br/com/fiaap/mspagamento/domain/dto/request/PagamentoRequest.java
+++ src/main/java/br/com/fiaap/mspagamento/domain/dto/request/PagamentoRequest.java
@@ -3,6 +3,7 @@ package br.com.fiaap.mspagamento.domain.dto.request;
 import br.com.fiaap.mspagamento.domain.Pagamento;
 import br.com.fiaap.mspagamento.domain.StatusPagamento;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -10,12 +11,26 @@ import java.util.UUID;
 
 public record PagamentoRequest(
 
+        @Schema(
+                description = "Identificador único do pedido",
+                example = "abf3a673-5211-483d-a25e-ce060baed2a0"
+        )
         @JsonProperty("id_pedido")
         UUID idPedido,
 
+        @Schema(
+                description = "Identificador único do cliente",
+                example = "1a3f0c88-7680-4c13-a439-802ca48f377c"
+        )
         @JsonProperty("id_cliente")
         UUID idCliente,
 
+        @Schema(
+                description = "Valor do pagamento",
+                example = "159.90",
+                minimum = "0.01",
+                required = true
+        )
         @JsonProperty("total")
         BigDecimal valorTotal
 ) {
@@ -24,7 +39,7 @@ public record PagamentoRequest(
                 pagamento.setDataPagamento(LocalDateTime.now());
                 pagamento.setIdCliente(idCliente);
                 pagamento.setIdPedido(idPedido);
-                pagamento.setStatusPagamento(StatusPagamento.RECEBIDO);
+                pagamento.setStatusPagamento(StatusPagamento.PENDENTE);
                 pagamento.setValorTotal(valorTotal);
                 return pagamento;
         }
