aad318d1ee39de2780fd22a261b260a1904823da
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-07-23 00:21:26 -0300

    Buscando o pagamentos do cliente

--- src/main/java/br/com/fiaap/mspagamento/repository/PagamentoRepository.java
+++ src/main/java/br/com/fiaap/mspagamento/repository/PagamentoRepository.java
@@ -1,9 +1,14 @@
 package br.com.fiaap.mspagamento.repository;
 
 import br.com.fiaap.mspagamento.domain.Pagamento;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PagamentoRepository extends MongoRepository<Pagamento, String> {
+
+    Page<Pagamento> findByIdCliente(String idCliente, Pageable pageable);
+
 }
